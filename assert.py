6d68ab285139d517f835439006ef30acc4cf66a4
Oleg Kiselyov <c0ncep2all@proton.me>
2023-01-16 14:47:06 +0600

    Basics

---
+++ assert.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import Select
+
+
+try:
+    link = "http://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    res = browser.find_element(By.CSS_SELECTOR, '.btn.btn-primary')
+    res.location_once_scrolled_into_view
+    res.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    # закрываем браузер после всех манипуляций
+    time.sleep(3)
+    browser.quit()
+
